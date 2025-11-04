33b36fed729ca81bc517ed4b4eb4e7a6f5ae3de1
H0rn0chse <h0rn0chse@gmx.de>
2025-11-04 21:32:13 +0100

    Initial draft

---
+++ src/main.js
@@ -0,0 +1,82 @@
+import { emojiCursor } from "./libs/emojiCursor.js";
+
+new emojiCursor({ emoji: ["❄"] });
+
+// document.getElementById("calendar").addEventListener("click", () => {
+//   alert("Hoho");
+// });
+
+const closeButton = document.querySelector("#close-btn");
+closeButton.addEventListener("click", () => {
+  const modal = document.getElementById("modal");
+  modal.close();
+  location.hash = "";
+});
+
+addEventListener("hashchange", () => {
+  const day = location.hash.substring(1);
+  if (day) {
+    openDay(day);
+  }
+});
+
+document.querySelectorAll(".day").forEach((dayElement) => {
+  dayElement.addEventListener("click", () => {
+    const day = dayElement.getAttribute("data-day");
+    location.hash = day;
+  });
+});
+
+function canOpenDay(day) {
+  const today = new Date().getDate();
+  return today >= day;
+}
+
+function fetchData() {
+  const sData = localStorage.getItem("adventskalender2025");
+  const initialData = {
+    openedDays: [],
+  };
+  try {
+    return JSON.parse(sData) || initialData;
+  } catch (e) {
+    return initialData;
+  }
+}
+
+const data = fetchData();
+
+function openDay(day) {
+  if (!canOpenDay(day)) {
+    location.hash = "";
+    alert("You cannot open this day yet!");
+    return;
+  }
+
+  if (!data.openedDays.includes(day)) {
+    data.openedDays.push(day);
+    saveData();
+    document.querySelector(`.day[data-day="${day}"]`).classList.add("opened");
+  }
+
+  const modal = document.getElementById("modal");
+  modal.showModal();
+}
+
+function saveData() {
+  localStorage.setItem("adventskalender2025", JSON.stringify(data));
+}
+
+// initialize already opened days
+document.querySelectorAll(".day").forEach((dayElement) => {
+  const day = dayElement.getAttribute("data-day");
+  if (data.openedDays.includes(day)) {
+    dayElement.classList.add("opened");
+  }
+});
+
+// open day if hash is present
+if (location.hash) {
+  const day = location.hash.substring(1);
+  openDay(day);
+}
